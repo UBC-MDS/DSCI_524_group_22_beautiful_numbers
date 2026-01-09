943aa7a3c24fd5820055a2aa63fd8070ac38217a
Jade <jade@dhcp-206-87-104-110.ubcsecure.wireless.ubc.ca>
2026-01-08 17:10:34 -0800

    add documentation for empty function fib_seq()

---
+++ src/beautifulnumbers/fib_seq.py
@@ -0,0 +1,23 @@
+def fib_seq(n):
+    """
+    Generates the Fibonacci sequence from 1 up to a given positive integer n.
+
+    Parameters
+    ----------
+    n : integer
+        The length of the Fibonacci sequence.
+
+    Returns
+    -------
+    list of int
+        The Fibonacci sequence of length n.
+
+    Examples
+    --------
+    >>> fib_seq(1)
+    [1]
+    >>> fib_seq(3)
+    [1,1,2]
+
+    """
+# TODO
