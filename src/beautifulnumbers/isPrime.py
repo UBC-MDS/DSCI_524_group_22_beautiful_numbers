bde1f519c9e16d2d866ffe6314f3ed356806e69a
Michael Oyatsi <oyatsimichael@gmail.com>
2026-01-08 17:21:36 -0800

    Create isPrime.py and populate docstrings of the function

---
+++ src/beautifulnumbers/isPrime.py
@@ -0,0 +1,20 @@
+def isPrime(n):
+    """
+    Return a boolean value for whether a number is a prime number. 
+    
+    Parameters:
+    -----------
+    n : integer
+    
+    Returns:
+    --------
+    Boolean 
+        A boolean value depending on whether the number is a prime number or not. 
+        
+    Examples: 
+    ---------
+    >>> isPrime(3)
+    True
+    >>> isPrime(6)
+    False
+    """
