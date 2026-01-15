d3caf2a812a79e65a7b5c193669e7e7c6242248a
Jade <jade@dhcp-206-12-177-68.ubcsecure.wireless.ubc.ca>
2026-01-14 16:06:13 -0800

    implement input check + exception handling, base cases, and bottom up approach

--- src/beautifulnumbers/fib_seq.py
+++ src/beautifulnumbers/fib_seq.py
@@ -20,4 +20,21 @@ def fib_seq(n):
     [1,1,2]
 
     """
-# TODO
+    # checks inputs & exception handling
+    if not isinstance(n, int):
+        raise TypeError("n must be an integer")
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+
+    # base case
+    if n == 1:
+        return [1]
+    if n == 2:
+        return [1, 1]
+
+    # bottom-up approach
+    seq = [1, 1]
+    for _ in range(2, n): # repeat this block n - 2 times (note: _ means ignored)
+        seq.append(seq[-1] + seq[-2])
+
+    return seq
